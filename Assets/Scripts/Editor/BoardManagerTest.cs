e59e9a3f8f0576c714899df994f69a8a6f0a5ddc
Niket Shah <niket.shah1@ocado.com>
2016-04-06 20:44:12 +0100

    Add road SVGs and layout grass and load level from JSON

---
+++ Assets/Scripts/Editor/BoardManagerTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class BoardManagerTest {
+
+    [Test]
+    public void LoadLevelFromFileTest()
+    {
+        //Arrange
+//        var gameObject = new GameObject();
+		var level = new BoardManager();
+
+        //Act
+        //Try to rename the GameObject
+//        var newGameObjectName = "My game object";
+//        gameObject.name = newGameObjectName;
+		var levelJSON = level.LoadJSONFile();
+
+        //Assert
+        //The object has a new name
+//        Assert.AreEqual(newGameObjectName, gameObject.name);
+		Assert.IsTrue (true);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public BoardManager boardScript;
+
+	private int level = 3;
+
+	// Use this for initialization
+	void Awake () {
+		boardScript = GetComponent<BoardManager> ();
+		InitGame ();
+	}
+
+	void InitGame() {
+		boardScript.SetupScene (level);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
