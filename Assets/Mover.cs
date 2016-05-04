b51266fa6f9240e57b750ac5bde5e95b2a7ecf1e
Niket Shah <masterniket@gmail.com>
2016-05-04 20:31:21 +0100

    Initial BoardManager code, road from json drawing

---
+++ Assets/Mover.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+
+    public float speed = 1.5f;
+    float timer = 0;
+    private Vector3 targetPosition;
+
+    public AnimationCurve motionCurve;
+
+    Vector3 startPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        startPosition = transform.position;
+        targetPosition = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime / 2;
+        float curvedValue = motionCurve.Evaluate(timer);
+        Vector3 newPos = Vector3.Lerp(startPosition, targetPosition, curvedValue);
+        transform.position = newPos;
+    }
+
+}
