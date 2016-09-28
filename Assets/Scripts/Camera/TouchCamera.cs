01e81387f77a4f34816428dc0b944d3d7015f07b
Niket Shah <niket.shah1@ocado.com>
2016-09-28 20:17:43 +0100

    Disable pinch to rotate

--- Assets/Scripts/Camera/TouchCamera.cs
+++ Assets/Scripts/Camera/TouchCamera.cs
@@ -46,7 +46,7 @@ public class TouchCamera : MonoBehaviour {
 				float newTouchDistance = newTouchVector.magnitude;
 
 				transform.position += transform.TransformDirection((Vector3)((oldTouchPositions[0] + oldTouchPositions[1] - screen) * GetComponent<Camera>().orthographicSize / screen.y));
-				transform.localRotation *= Quaternion.Euler(new Vector3(0, 0, Mathf.Asin(Mathf.Clamp((oldTouchVector.y * newTouchVector.x - oldTouchVector.x * newTouchVector.y) / oldTouchDistance / newTouchDistance, -1f, 1f)) / 0.0174532924f));
+				// transform.localRotation *= Quaternion.Euler(new Vector3(0, 0, Mathf.Asin(Mathf.Clamp((oldTouchVector.y * newTouchVector.x - oldTouchVector.x * newTouchVector.y) / oldTouchDistance / newTouchDistance, -1f, 1f)) / 0.0174532924f));
 				GetComponent<Camera>().orthographicSize *= oldTouchDistance / newTouchDistance;
 				transform.position -= transform.TransformDirection((newTouchPositions[0] + newTouchPositions[1] - screen) * GetComponent<Camera>().orthographicSize / screen.y);
 
