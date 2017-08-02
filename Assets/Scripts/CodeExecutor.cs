913c1f1bdf2467b9d232f4148dc2ccfab07943ef
Jackwgm <jwgmitchell@gmail.com>
2017-08-02 20:26:38 +0100

    Moved logic for running user-defined code to a new class (#92)
    * Move logic for running user-defined code to a new class

---
+++ Assets/Scripts/CodeExecutor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using UnityEngine;
+using Zenject;
+
+public class CodeExecutor : MonoBehaviour
+{
+	[Inject]
+	VehicleMover vehicleMover;
+
+	public void Run(Code code) {
+		Google.Protobuf.Collections.RepeatedField<Method> methods = code.Methods;
+
+		Method startMethod = GetStart (methods);
+
+		Call (startMethod);
+	}
+
+	private Method GetStart(Google.Protobuf.Collections.RepeatedField<Method> methods) {
+		foreach (Method method in methods) {
+			if (method.Name.Equals("Start")) {
+				return method;
+			}
+		}
+		throw new Exception ("No Start method found");
+	}
+
+	private void Call(Method method) {
+		foreach (Instruction instruction in method.Instructions) {
+			FollowInstruction (instruction);
+		}
+	}
+
+	private void FollowInstruction(Instruction instruction) {
+		switch (instruction.Type) {
+		case Instruction.Types.Type.Call:
+			// TODO Call ();
+			break;
+		case Instruction.Types.Type.Deliver:
+			// TODO Deliver ();
+			break;
+		case Instruction.Types.Type.IfDo:
+			// TODO IfDo ();
+			break;
+		case Instruction.Types.Type.IfDoElse:
+			// TODO IfDoElse ();
+			break;
+		case Instruction.Types.Type.MoveForwards:
+			vehicleMover.StartForward ();
+			break;
+		case Instruction.Types.Type.RepeatTimesDo:
+			// TODO RepeatTimesDo ();
+			break;
+		case Instruction.Types.Type.RepeatUntilDo:
+			// TODO RepeatUntilDo ();
+			break;
+		case Instruction.Types.Type.RepeatWhileDo:
+			// TODO RepeatWhileDo ();
+			break;
+		case Instruction.Types.Type.TurnAround:
+			// TODO TurnAround ();
+			break;
+		case Instruction.Types.Type.TurnLeft:
+			vehicleMover.StartLeft ();
+			break;
+		case Instruction.Types.Type.TurnRight:
+			vehicleMover.StartRight ();
+			break;
+		case Instruction.Types.Type.Wait:
+			// TODO Wait ();
+			break;
+		}
+	}
+}
+
