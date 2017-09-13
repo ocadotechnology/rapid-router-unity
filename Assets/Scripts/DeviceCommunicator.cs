6e37d4a22d53be96b39c1c7c0afb0dcb9e48d2f0
Jacq Train <jacqtrain@gmail.com>
2017-09-13 20:03:35 +0100

    Issue #81 Add external method to send end level message with finished status (SUCCESSFUL / FAILED enum) to external listener

---
+++ Assets/Scripts/DeviceCommunicator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using UnityEngine;
+using Zenject;
+
+public class DeviceCommunicator : MonoBehaviour {
+
+	[Inject]
+	CodeExecutor codeExecutor;
+
+	public void Listen(string protobufInstructionsString) 
+	{
+		byte[] protobufInstructions = Encoding.Default.GetBytes (protobufInstructionsString);
+		Code code = Code.Parser.ParseFrom (protobufInstructions);
+
+		codeExecutor.Run (code);
+	}
+
+	[DllImport ("__Internal")]
+	private extern static void endLevel(LevelCompleteStatus status);
+
+	public void EndLevel(LevelCompleteStatus status) 
+	{
+		endLevel(status);
+	}
+}
---
+++ Assets/Scripts/Editor/DeviceCommunicatorTest.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Tests")]
+
+namespace Tests
+{
+    public class BoardTranslatorTest
+    {
+        [Test]
+        public void CheckEndLevelMessageBroadcasted()
+        {
+            
+        }
+    }
+}
